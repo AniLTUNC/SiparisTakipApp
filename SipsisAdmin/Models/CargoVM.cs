5dc638aa426e9e69e4d425d290e67bcb506b592b
Muhammed Anıl <111355066+AniLTUNC@users.noreply.github.com>
2022-11-27 20:32:01 +0300

    Add files via upload

---
+++ SipsisAdmin/Models/CargoVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SipsisAdmin.Models
+{
+    public class CargoVM
+    {
+        public int Id { get; set; }
+        public string CargoName { get; set; }
+        public string CargoImageURL { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
---
+++ SipsisAdmin/Models/CustomerVM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SipsisAdmin.Models
+{
+    public class CustomerVM
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerAddress { get; set; }
+        public int UserId { get; set; }
+        public int MarketId { get; set; }
+    }
+}
